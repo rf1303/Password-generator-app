573872487404bb1765cb0c2bb48d7e6ed7a27d95
Ramiro Fernandez <pulgaramiro@gmail.com>
2025-04-25 16:34:51 -0300

    Range Track Terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,18 @@ document.addEventListener('DOMContentLoaded', function () {
     const generatorBar = document.querySelector('.generator__bar');
     const inputRange = document.querySelector('.bar__range');
     const lengthNumber = document.querySelector('.length__number');
-    let generatorBarWidth = generatorBar.offsetWidth;
+    /* let generatorBarWidth = generatorBar.offsetWidth;  */
          
 
-    const trackWidth = new ResizeObserver(entries => {
-        for(let entry of entries) {
-            generatorBarWidth = entry.contentRect.width;
-            console.log(`Ancho de .generator__bar: ${generatorBarWidth}px`);
-            /* colorTrack(); */
-        }
-    });
-
-    trackWidth.observe(generatorBar);
-    console.log("el width", trackWidth);
+    // const trackWidth = new ResizeObserver(entries => {
+    //     for(let entry of entries) {
+    //         generatorBarWidth = entry.contentRect.width;
+    //         console.log(`Ancho de .generator__bar: ${generatorBarWidth}px`);
+    //         /* colorTrack(); */
+    //     }
+    // });
+    // trackWidth.observe(generatorBar);
+    // console.log("el width", trackWidth);
 
     inputRange.addEventListener('input', () => {
         const value = inputRange.value;
@@ -23,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const trackValue = (value / 20) * 100;
         console.log("trackValue: ", trackValue);
         console.log("inputRange: ", value);
-        document.body.style.setProperty('--track-position', `${trackValue}%`);
+        inputRange.style.setProperty('--track-position', `${trackValue}%`);
         lengthNumber.textContent = `${value}`;
     });
 
