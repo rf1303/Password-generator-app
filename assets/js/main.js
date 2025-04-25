6b96be29234e53b570c29ea2b4c3d7b12402ff97
Ramiro Fernandez <pulgaramiro@gmail.com>
2025-04-25 11:45:19 -0300

    terminando input range track color

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
-  
+    
+    const generatorBar = document.querySelector('.generator__bar');
     const inputRange = document.querySelector('.bar__range');
 
+    let generatorBarWidth = generatorBar.offsetWidth;
+         
+
+    const trackWidth = new ResizeObserver(entries => {
+        for(let entry of entries) {
+            generatorBarWidth = entry.contentRect.width;
+            console.log(`Ancho de .generator__bar: ${generatorBarWidth}px`);
+            /* colorTrack(); */
+        }
+    });
+
+    trackWidth.observe(generatorBar);
+    console.log("el width", trackWidth);
+
     inputRange.addEventListener('input', () => {
-        console.log("range");
         const value = inputRange.value;
+        console.log("inputRange: ", value);
         document.body.style.setProperty('--track-position', `${value}%`);
         /* colorRange(inputRange); */
     });
-
-    // function colorRange(slider) {
-    //   const value = (slider.value - slider.min) / (slider.max - slider.min) * 100;
-    //   slider.style.background = `linear-gradient(to right, #a4ffaf ${value}%, #2a2a2a ${value}%)`;
+    
+    // function colorTrack() {
+    //     const range = +inputRange.value;
+    //     console.log('colorTrack : ', range)
+    //     /* const trackPos = (range / 100) * generatorBarWidth; */
+    //
+    //     /* inputRange.style.background = `linear-gradient(90deg, #a4ffaf ${trackpos}px, transparent 0)`; */
+    //      
     // }
 
-     /* colorRange(inputRange); */
-    
 });
