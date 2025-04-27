e37ea75143d4e558c94492c88cd34531edc8cbe4
Ramiro Fernandez <pulgaramiro@gmail.com>
2025-04-27 10:14:57 -0300

    Terminando Generator

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,75 @@
 document.addEventListener('DOMContentLoaded', function () {
     
-    const generatorBar = document.querySelector('.generator__bar');
+    let numberPassword = 10;
+    let passwordDates = "";
+    let passwordGenerator = "";
+    let includePasswords = {}
+    const generatorTitle = document.querySelector('.generator__title')
+    const generatorInclude = document.querySelector('.generator__include'); 
+    const generatorButton = document.querySelector('.button__generate');
     const inputRange = document.querySelector('.bar__range');
     const lengthNumber = document.querySelector('.length__number');
-    /* let generatorBarWidth = generatorBar.offsetWidth;  */
-         
+    const includeGenerator = {
+        includeUpper: "AOEUIDRTNSZVWMBXKJQPYFGGCHL",
+        includeLower: "qjkxbmwvzaoeuidrtnslhcgfyp",
+        includeNumber: "1234567890",
+        includeSymbol: "!#$%&/()=?¡¿+*}{][.:,;-_<>"
+    };
+    const includeregex = {
+                          uppers: /[A-Z]/,
+                          lowers: /[a-z]/,
+                          numbers: /[0-9]/,
+                          symbols: /[|°!#$%&/()=?¡*+}{[]-_<>,;.:¿]/
+                         };
 
-    // const trackWidth = new ResizeObserver(entries => {
-    //     for(let entry of entries) {
-    //         generatorBarWidth = entry.contentRect.width;
-    //         console.log(`Ancho de .generator__bar: ${generatorBarWidth}px`);
-    //         /* colorTrack(); */
-    //     }
-    // });
-    // trackWidth.observe(generatorBar);
-    // console.log("el width", trackWidth);
 
     inputRange.addEventListener('input', () => {
-        const value = inputRange.value;
-        /* const track = (value - min) / (max - min) * 100; */
-        const trackValue = (value / 20) * 100;
-        console.log("trackValue: ", trackValue);
-        console.log("inputRange: ", value);
+        numberPassword = inputRange.value;
+        const trackValue = (numberPassword / 20) * 100;
         inputRange.style.setProperty('--track-position', `${trackValue}%`);
-        lengthNumber.textContent = `${value}`;
+        lengthNumber.textContent = `${numberPassword}`;
+        console.log('nro passwd: ', numberPassword);
     });
+
+    generatorInclude.addEventListener('change', (e) => {
+        console.log('generatorInclude: ', e.target.type);
+        includePasswords = {  
+            includeUpper: document.getElementById('upper').checked,
+            includeLower: document.getElementById('lower').checked,
+            includeNumber:  document.getElementById('incNum').checked,
+            includeSymbol:  document.getElementById('incSym').checked,
+        }
+            console.log( includePasswords.includeUpper, includePasswords.includeLower, includePasswords.includeNumber, includePasswords.includeSymbol);
+        console.log(passwordGenerator);
+    });
+
+    generatorButton.addEventListener('click', () => {
+        console.log('click: ', numberPassword, includePasswords)
+        const includeButton = Object.values(includePasswords).some(value => value); 
+        console.log(includeButton);
+        if (numberPassword !== 0 && includeButton ) {
+           includeFilter(numberPassword, includePasswords); 
+        } 
+    });
+
+
+    function includeFilter(number, includes) {
+        passwordGenerator = "";
+        //obtiene los indices de includePasswords los filtra y +includeGenerator
+       passwordDates = Object.keys(includes) 
+                    .filter(include => includes[include])
+                    .map(include => includeGenerator[include])
+                    .join('')
+        console.log('passwordDates: ',passwordDates);
+        for (let i = 0; i < number; i++) {
+            const randomPasswd = Math.floor(Math.random() * passwordDates.length);
+            passwordGenerator += passwordDates[randomPasswd]; 
+        }
+        console.log('includeFilter: ', passwordGenerator);
+        generatorTitle.textContent = `${passwordGenerator}`;
+        const passwordStrength = zxcvbn(passwordGenerator);
+        console.log(passwordStrength.score);
+        return passwordGenerator;
+    }
 
 });
